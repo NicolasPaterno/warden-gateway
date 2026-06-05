214e1dc5b7b6349b6034299fcd9fd7e720f2a915
NicolasPaterno <nicolasps.contato@gmail.com>
2026-06-04 22:11:32 -0300

    feat(nats): add reconnect logic with retry and connection handlers

--- internal/nats/publisher.go
+++ internal/nats/publisher.go
@@ -3,6 +3,8 @@ package nats
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"time"
 
 	natsgo "github.com/nats-io/nats.go"
 	warden "github.com/nicaozx/warden-gateway"
@@ -13,7 +15,20 @@ type Publisher struct {
 }
 
 func NewPublisher(url string) (*Publisher, error) {
-	conn, err := natsgo.Connect(url)
+	conn, err := natsgo.Connect(
+		url,
+		natsgo.ReconnectHandler(func(conn *natsgo.Conn) {
+			log.Printf("Reconnected to NATS: %v", conn.ConnectedUrl())
+		}),
+		natsgo.MaxReconnects(10),
+		natsgo.ReconnectWait(5*time.Second),
+		natsgo.RetryOnFailedConnect(true),
+		natsgo.ConnectHandler(func(conn *natsgo.Conn) {
+			log.Printf("Connected to NATS: %v", conn.ConnectedUrl())
+		}),
+		natsgo.DisconnectErrHandler(func(conn *natsgo.Conn, err error) {
+			log.Printf("Disconnected from NATS: %v", err)
+		}))
 	if err != nil {
 		return nil, err
 	}
